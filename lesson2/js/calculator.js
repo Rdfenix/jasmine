8ca3d28edcbaed9e4255b2f01c786b59c8e9321c
Rudnei Oliveira <rudnei.oliveira@i-value.int>
2016-04-20 14:09:00 -0300

    Estudo sobre jasmine

--- lesson2/js/calculator.js
+++ lesson2/js/calculator.js
@@ -1,5 +1,13 @@
 window.Calculator = {
+  current: 0,
+
   add: function(num1){
-      return 0 + num1;
+    var sum = this.current;
+
+    for (var i = 0; i < arguments.length; i++) {
+      sum += arguments[i];
+    }
+      this.current = sum;
+      return this.current;
   }
 };
--- lesson2/spec/calculator_spec.js
+++ lesson2/spec/calculator_spec.js
@@ -1,7 +1,18 @@
 describe('Calculator', function(){
+  beforeEach(function(){
+    Calculator.current = 0;
+  });
+  it("should store the current value at all times", function(){
+    //expect(Calculator.current).tobeDefined();
+    expect(Calculator.current).toEqual(0);
+  });
   it("should add numbers", function(){
       // Calculator é um objeto
       expect(Calculator.add(5)).toEqual(5);
-      //expect(Calculator.add(5)).toEqual(10);
-  })
+      expect(Calculator.add(5)).toEqual(10);
+  });
+  it("should add any number of numbers", function(){
+    expect(Calculator.add(1,2,3)).toEqual(6);
+    expect(Calculator.add(1,2,3,4)).toEqual(16);
+  });
 });
